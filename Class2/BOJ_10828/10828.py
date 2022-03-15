4883475fd9318d4a9d138284a3c1f35bf46593bc
DESKTOP-1NGUJ0F\admin <arkanum3608@naver.com>
2022-03-15 12:27:23 +0900

    10845_stack

---
+++ Class2/BOJ_10828/10828.py
@@ -0,0 +1,30 @@
+#SILVER 4 # Stack
+import sys
+
+N = int(sys.stdin.readline())
+
+
+stack = []
+for _ in range(N):
+    k = sys.stdin.readline().split()
+    if k[0] == "push":
+        stack.append(k[1])
+    elif k[0] == "pop":
+        if len(stack) == 0:
+            print(-1)
+        else:
+            print(stack.pop())
+    elif k[0] == "size":
+        print(len(stack))
+    elif k[0] == "empty":
+        if len(stack) == 0:
+            print(1)
+        else:
+            print(0)    
+    elif k[0] == "top":
+        if len(stack) == 0:
+            print(-1)
+        else:
+            print(stack[len(stack)-1])
+
+# 10845와 동일
