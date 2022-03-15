8ed4a90f4b5a97ee8c9c71abbda3c5e7a1d96f78
DESKTOP-1NGUJ0F\admin <arkanum3608@naver.com>
2022-03-15 11:57:56 +0900

    10845 queue

---
+++ Class2/BOJ_10845/10845.py
@@ -0,0 +1,34 @@
+#SILVER 4 #queue
+import sys
+N = int(sys.stdin.readline())
+
+queue = []
+for i in range(N):
+    k = sys.stdin.readline().split()
+    if k[0] == "push":
+        queue.append(k[1])
+    elif k[0] == "front":
+        if len(queue) == 0 :
+            print(-1)
+        else:
+            print(queue[0])
+    elif k[0] == "back":
+        if len(queue) == 0:
+            print(-1)
+        else:
+            print(queue[len(queue)-1])
+    elif k[0] == "size":
+        print(len(queue))
+    elif k[0] == "pop":
+        if len(queue) != 0 :
+            print(queue.pop(0))
+        else:
+            print(-1)
+    elif k[0] == "empty":
+        if len(queue) == 0:
+            print(1)
+        else:
+            print(0)
+
+# collections library 굳이 안써도 stack 구조에 대한 이해
+# pop() & pop(0),  insert(0,k[0]) & append(k[0]) 차이 이해
