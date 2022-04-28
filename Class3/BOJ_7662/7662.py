802b4cffe86987ff17e04ad07a81e28358566cb5
DESKTOP-1NGUJ0F\admin <arkanum3608@naver.com>
2022-04-28 10:40:28 +0900

    7762_다시풀기

--- Class3/BOJ_7662/7662.py
+++ Class3/BOJ_7662/7662.py
@@ -1,31 +1,38 @@
+#_7662
+#class3 gold4
+#heapq 활용 + visited 인덱스 -> 포인터느낌으로 사용하기
+
 import sys
 import heapq
-
-
 T = int(sys.stdin.readline())
-heap = []
-
 for _ in range(T):
     k = int(sys.stdin.readline())
-    for _ in range(k):
+    visitied = [False]*1000001 #visited를 따로만들어서 두는게 중요함
+    min_heap,max_heap = [],[]
+    for i in range(k):
         cmd = list(sys.stdin.readline().split())
-        cmd[1] = int(cmd[1])
         if cmd[0] == 'I':
-            heapq.heappush(heap,cmd[1])
-        elif cmd[0] == 'D':
-            if cmd[1] == 1:
-                if len(heap) == 0:
-                    continue
-                else:
-                    heap = heapq.nlargest(len(heap),heap)[1:] #최대값 제거
-                    heapq.heapify(heap)
-            elif cmd[1] == -1:
-                if len(heap) == 0:
-                    continue
-                else:
-                    heapq.heappop(heap) #최소값 제거
-                    
-    if len(heap) == 0:
-        print('EMPTY')
+            heapq.heappush(min_heap,(int(cmd[1]),i))
+            heapq.heappush(max_heap,(-int(cmd[1]),i))
+            visitied[i] = True  
+        else:
+            if cmd[1] == "-1":
+                while min_heap and not visitied[min_heap[0][1]]:
+                    heapq.heappop(min_heap)
+                if min_heap: 
+                    visitied[min_heap[0][1]] = False
+                    heapq.heappop(min_heap)
+            if cmd[1] == "1":
+                while max_heap and not visitied[max_heap[0][1]]:
+                    heapq.heappop(max_heap)
+                if max_heap:
+                    visitied[max_heap[0][1]] = False
+                    heapq.heappop(max_heap)
+    while min_heap and not visitied[min_heap[0][1]]:
+        heapq.heappop(min_heap)
+    while max_heap and not visitied[max_heap[0][1]]:
+        heapq.heappop(max_heap)
+    if len(min_heap)==0 or len(max_heap) == 0:
+        print("EMPTY")
     else:
-        print(heapq.nlargest(len(heap),heap)[0],heapq.heappop(heap))
+        print(-max_heap[0][0], min_heap[0][0])
